45156464fd4479bde433a4387ffdbc79ed9bbb31
Anya Yao <anya.yao23@gmail.com>
2024-07-24 20:35:15 -0400

    Add server

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
 @app.route("/emotionDetector")
 def sent_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
@@ -19,3 +23,6 @@ def sent_analyzer():
 
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}} and 'sadness': {}}. The dominant emotion is {}}."
             .format(anger, disgust, fear, joy, sadness, dominant_emotion)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
